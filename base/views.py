c94e4baf4f9a3c3735ecf936b04de879160defba
Octavianne <110049794+Octavianne@users.noreply.github.com>
2023-03-22 13:33:09 +0200

    blogmontan

---
+++ base/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm
+
+
+# Create your views here.
+
+
+# rooms = [
+#     {'id':1, 'name':'Urcare Vf. Omu'},
+#     {'id':2, 'name':'Recenzii bocanci munte'},
+#     {'id':3, 'name':'Detalii trasee inchise'},
+# ]
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context )
+
+
+def room(request, pk):
+
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return rediret('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
